1ff8191d59fec0de1827ea79df0e93d1ce20744f
Kas-ES <kasah19@student.sdu.dk>
2021-11-02 16:37:02 +0100

    CNN

--- NerualNetworkCNN.py
+++ NerualNetworkCNN.py
@@ -1,16 +1,168 @@
 import os
 import sys
 
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers, regularizers
 from tensorflow.keras.datasets import cifar10
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
 
-x_train = x_train.reshape(-1, 32*32*3).astype("float32") / 255.0
-x_test = x_test.reshape(-1, 32*32*3).astype("float32") / 255.0
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
 
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=(32, 32, 3)),
+        layers.Conv2D(32, 3, padding="valid", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(128, 3, activation="relu"),
+        layers.Flatten(),
+        layers.Dense(64, activation="relu"),
+        layers.Dense(10),
+    ]
+)
+
+#A MODEL WITH MORE CONV LAYERS AND DENSE LAYERS, AND COPY THAT AND TRY ELU ACTIVATION
+
+def model_MoreSmallCONVlayers():
+    inputs = keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),)(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(24, 3, padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01), )(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(64, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),)(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(24, 3, padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01), )(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(
+        168, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),)(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Flatten()(x)
+
+    x = layers.Dense(64, activation="relu", kernel_regularizer=regularizers.l2(0.01),)(x)
+    x = layers.Dropout(0.25)(x)
+    outputs = layers.Dense(10)(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def model_MoreCONVlayers():
+    inputs = keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),)(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(64, 3, padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01), )(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(128, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),)(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(168, 3, padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01), )(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(
+        192, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),)(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Flatten()(x)
+
+    x = layers.Dense(64, activation="relu", kernel_regularizer=regularizers.l2(0.01),)(x)
+    x = layers.Dropout(0.25)(x)
+    outputs = layers.Dense(10)(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def model_Regularizer_DropOut():
+    inputs = keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),)(
+        inputs
+    )
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(64, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),)(
+        x
+    )
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(
+        128, 3, padding="same", activation="relu" ,kernel_regularizer=regularizers.l2(0.01),
+    )(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(64, activation="relu", kernel_regularizer=regularizers.l2(0.01),)(
+        x
+    )
+    x = layers.Dropout(0.25)(x)
+    outputs = layers.Dense(10)(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+def Simple_model():
+    inputs = keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, 3, padding="same", activation="relu")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(64, 3, activation="relu")(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(128, 3, activation="relu")(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(64, activation="relu")(x)
+    outputs = layers.Dense(10)(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+model = model_MoreSmallCONVlayers()
+
+
+print(model.summary())
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(learning_rate=3e-4),
+    metrics=["accuracy"],
+)
+
+model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+model.evaluate(x_test, y_test, batch_size=64, verbose=2)
